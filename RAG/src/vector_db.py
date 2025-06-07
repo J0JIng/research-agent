4cc57e4cdaef56a45217e245473259419b558e3f
Jing <ojing12321@gmail.com>
2025-06-07 20:17:21 +0800

    debug

--- RAG/src/vector_db.py
+++ RAG/src/vector_db.py
@@ -6,7 +6,6 @@ from chromadb import PersistentClient
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
-from langchain_core.vectorstores import VectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
 
@@ -37,43 +36,62 @@ class ChromaVectorDB(BaseVectorDB):
         self.embedding_function = embedding_function  
         self.collection_name = collection_name
         self.filepath = filepath
-        self.uuids = {}
+        
+        #TODO - need to store UUID of each chunk for update and delete operation. 
+        # self.uuids = set()  
 
         self.client = PersistentClient(path=self.filepath)
         self.collection = self.client.get_or_create_collection(name=self.collection_name)
         self.create()
 
     def create(self) -> None:
-        self.vector_store = Chroma(
-            client = self.client,
-            collection_name = self.collection_name,
-            embedding_function = self.embedding_function,
-        )
+        try: 
+            self.vector_store = Chroma(
+                client = self.client,
+                collection_name = self.collection_name,
+                embedding_function = self.embedding_function,
+            )
+        except Exception as e:
+            print(f"Exception in chroma vector store: {e}")
 
     def store(self, uuids: list[str], documents: list[Document]) -> None:
-        # update exisitng uuids
-        
-        self.vector_store.add_documents(
-            ids = uuids,
-            documents = documents,
-        )
-        
+        try:
+            self.vector_store.add_documents(
+                ids = uuids,
+                documents = documents,
+            )
+        except Exception as e:
+            print(f"Exception in storing documents: {e}")
+    
+
     def update(self, uuids: list[str], documents: list[Document]) -> None:
-        self.vector_store.update_documents(
-            ids = uuids, 
-            documents = documents,
-        )
+        try: 
+            self.vector_store.update_documents(
+                ids = uuids, 
+                documents = documents,
+            )
+        except Exception as e:
+            print(f"Exception in updating documents: {e}")
 
     def delete(self, uuids: list[str]) -> None:
-        self.collection.delete(
-            ids = uuids
-        )
+        try: 
+            self.collection.delete(
+                ids = uuids
+            )
+        except Exception as e:
+            print(f"Exception in deleting documents: {e}")
 
-    def query(self, query_texts: str, n_results: int = 4, filter: Optional[dict[str, str]] = None) -> list[tuple[Document, float]]:
-        result = self.vector_store.similarity_search_with_score(
-            query = query_texts,
-            k = n_results,
-            filter = filter,
-        )
-        return result
+    def query(self, query_text: str, n_results: int = 4, filter: Optional[dict[str, str]] = None) -> list[tuple[Document, float]] | None:
+        try: 
+            result = self.vector_store.similarity_search_with_score(
+                query = query_text,
+                k = n_results,
+                filter = filter,
+            )
+            return result
+        except Exception as e:
+            print(f"Exception in querying documents: {e}")
 
+if __name__ == "__main__":
+    embedding = OpenAIEmbeddings()
+    db = ChromaVectorDB(embedding, "research_docs", "./chromadb")
